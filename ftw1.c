d2673ca88ec1adb9740d1bfba5ddcbcdf3f968ef
Tal <talzhtc@gmail.com>
2021-11-24 20:22:12 +0200

    Removed printing hidden files and added group name

--- ftw1.c
+++ ftw1.c
@@ -3,6 +3,8 @@
 #include <string.h>
 #include <stdio.h>
 #include <sys/stat.h>
+#include <sys/types.h>
+#include <grp.h>
 
 int list(const char *name, const struct stat *status, int type);
 
@@ -15,23 +17,46 @@ int main(int argc, char *argv[]) {
 }
 
 int list(const char *name, const struct stat *status, int type) {
+
+   // if file is of unknown type, return 
    if(type == FTW_NS)
       return 0;
+   
+   // if file is hidden, return 
+   if(name[2] == '.')
+      return 0;
+
 
    if(type == FTW_F){
+      
+      // declare necessary variables
       uid_t user_id = status->st_uid;
-      struct passwd *pws;
-      pws = getpwuid(user_id);
+      gid_t gid = status->st_gid;
+      off_t size = status->st_size;
+      char *permissions = "----------";
+
+      // get user name
+      struct passwd *pws = getpwuid(user_id);
       char *user_name = pws->pw_name;
 
-      printf("0%3o\t%s\t%s\n", status->st_mode & 0777, name, user_name);
+      // get group name
+      struct group *grp = getgrgid(gid);
+      char *group_name = grp->gr_name;
 
+      printf("0%3o\t%s\t%s\t%s\n", status->st_mode & 0777, name, user_name, group_name);
    }
       
       
  
-   if(type == FTW_D && strcmp(".", name) != 0)
-      printf("0%3o\t%s/\n", status->st_mode&0777, name);
+   else if(type == FTW_D && strcmp(".", name) != 0){
+      uid_t user_id = status->st_uid;
+      gid_t gid = status->st_gid;
+      off_t size = status->st_size;
+      char *permissions = "d---------";
+
+      printf("0%3o\t%s/\n", status->st_mode & 0777, name);
+   }
+      
 
    return 0;
 }
