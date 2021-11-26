fd34cb2df2d4e9a1ca66099bc3c5dc8f33ce72b9
Tal <talzhtc@gmail.com>
2021-11-26 12:03:31 +0200

    Printing correctly without indention

--- ftw1.c
+++ ftw1.c
@@ -27,14 +27,14 @@ int list(const char *name, const struct stat *status, int type) {
       return 0;
 
 
-   if(type == FTW_F){
+   if(type == FTW_F || type == FTW_D){
       
       // declare necessary variables
       uid_t user_id = status->st_uid;
       gid_t gid = status->st_gid;
       off_t size = status->st_size;
       size_t path_len = strlen(name);
-      char permissions[11];
+      char permissions[10];
       char file_name[path_len];
 
       // get user name
@@ -45,17 +45,7 @@ int list(const char *name, const struct stat *status, int type) {
       struct group *grp = getgrgid(gid);
       char *group_name = grp->gr_name;
 
-      // get file name
-      int name_start_index = 0;
-      for(size_t i = path_len-1; name[i] != '/' ; --i){
-         name_start_index++;
-      }
-      name_start_index = path_len - name_start_index;
-      strncpy(file_name, name+name_start_index, path_len-1);
-
-      
-      // get permissions
-      permissions[0] = '-';
+      permissions[0] = type == FTW_F ? '-' : 'd';
       permissions[1] = status->st_mode & S_IRUSR ? 'r' : '-';
       permissions[2] = status->st_mode & S_IWUSR ? 'w' : '-';
       permissions[3] = status->st_mode & S_IXUSR ? 'x' : '-';
@@ -66,63 +56,21 @@ int list(const char *name, const struct stat *status, int type) {
       permissions[8] = status->st_mode & S_IWOTH ? 'w' : '-';
       permissions[9] = status->st_mode & S_IXOTH ? 'x' : '-';
 
-
-      printf("[%s %s %s\t\t%lld]  %s\n", permissions, user_name, group_name, size, file_name);
-   }
-
-   else if(type == FTW_D){
-      // declare necessary variables
-      uid_t user_id = status->st_uid;
-      gid_t gid = status->st_gid;
-      off_t size = status->st_size;
-      size_t path_len = strlen(name);
-      char permissions[11];
-      char file_name[path_len];
-
-      // get user name
-      struct passwd *pws = getpwuid(user_id);
-      char *user_name = pws->pw_name;
-
-      // get group name
-      struct group *grp = getgrgid(gid);
-      char *group_name = grp->gr_name;
-
-      // get file name
-      int name_start_index = 1;
-      for (size_t i = path_len - 2; name[i] != '/'; --i)
-      {
-         name_start_index++;
+      // parse file name
+      if(path_len == 1){
+         strcpy(file_name, ".");
       }
-      name_start_index = path_len - name_start_index;
-      strncpy(file_name, name + name_start_index, path_len - 2);
-
-      // get permissions
-      permissions[0] = 'd';
-      permissions[1] = status->st_mode & S_IRUSR ? 'r' : '-';
-      permissions[2] = status->st_mode & S_IWUSR ? 'w' : '-';
-      permissions[3] = status->st_mode & S_IXUSR ? 'x' : '-';
-      permissions[4] = status->st_mode & S_IRGRP ? 'r' : '-';
-      permissions[5] = status->st_mode & S_IWGRP ? 'w' : '-';
-      permissions[6] = status->st_mode & S_IXGRP ? 'x' : '-';
-      permissions[7] = status->st_mode & S_IROTH ? 'r' : '-';
-      permissions[8] = status->st_mode & S_IWOTH ? 'w' : '-';
-      permissions[9] = status->st_mode & S_IXOTH ? 'x' : '-';
-
-      printf("[%s %s %s\t\t%lld]  %s\n", permissions, user_name, group_name, size, file_name);
-   }
+      else{
+         int file_name_len = 0;
+         for(size_t i = path_len-1; name[i] != '/'; --i)
+         {
+            ++file_name_len;
+         }
+         int name_begin_index = path_len - file_name_len;
+         strncpy(file_name, name+name_begin_index, path_len-1);
+      }
+      printf("[%s %s %s\t\t%ld]  %s\n", permissions, user_name, group_name, size, file_name);
       
-
+   }
    return 0;
 }
-
-// FTW_F    The object is a  file
-// FTW_D    ,,    ,,   ,, ,, directory
-// FTW_DNR  ,,    ,,   ,, ,, directory that could not be read
-// FTW_SL   ,,    ,,   ,, ,, symbolic link
-// FTW_NS   The object is NOT a symbolic link and is one for 
-//          which stat() could not be executed
-
-
-/*
-   condition ? if true : else
-*/
