b25d894f4a47d8c069686dc056a63df013525e83
Tal <talzhtc@gmail.com>
2021-11-26 11:28:15 +0200

    checking on mac

--- ftw1.c
+++ ftw1.c
@@ -34,7 +34,7 @@ int list(const char *name, const struct stat *status, int type) {
       gid_t gid = status->st_gid;
       off_t size = status->st_size;
       size_t path_len = strlen(name);
-      char *permissions = "----------";
+      char permissions[11];
       char file_name[path_len];
 
       // get user name
@@ -45,25 +45,70 @@ int list(const char *name, const struct stat *status, int type) {
       struct group *grp = getgrgid(gid);
       char *group_name = grp->gr_name;
 
-      //getting file name
+      // get file name
       int name_start_index = 0;
       for(size_t i = path_len-1; name[i] != '/' ; --i){
          name_start_index++;
       }
       name_start_index = path_len - name_start_index;
       strncpy(file_name, name+name_start_index, path_len-1);
-      printf("0%3o\t%s\t%s\t%s\n", status->st_mode & 0777, file_name, user_name, group_name);
+
+      
+      // get permissions
+      permissions[0] = '-';
+      permissions[1] = status->st_mode & S_IRUSR ? 'r' : '-';
+      permissions[2] = status->st_mode & S_IWUSR ? 'w' : '-';
+      permissions[3] = status->st_mode & S_IXUSR ? 'x' : '-';
+      permissions[4] = status->st_mode & S_IRGRP ? 'r' : '-';
+      permissions[5] = status->st_mode & S_IWGRP ? 'w' : '-';
+      permissions[6] = status->st_mode & S_IXGRP ? 'x' : '-';
+      permissions[7] = status->st_mode & S_IROTH ? 'r' : '-';
+      permissions[8] = status->st_mode & S_IWOTH ? 'w' : '-';
+      permissions[9] = status->st_mode & S_IXOTH ? 'x' : '-';
+
+
+      printf("[%s %s %s\t\t%lld]  %s\n", permissions, user_name, group_name, size, file_name);
    }
-      
-      
- 
-   else if(type == FTW_D && strcmp(".", name) != 0){
+
+   else if(type == FTW_D){
+      // declare necessary variables
       uid_t user_id = status->st_uid;
       gid_t gid = status->st_gid;
       off_t size = status->st_size;
-      char *permissions = "d---------";
+      size_t path_len = strlen(name);
+      char permissions[11];
+      char file_name[path_len];
 
-      printf("0%3o\t%s/\n", status->st_mode & 0777, name);
+      // get user name
+      struct passwd *pws = getpwuid(user_id);
+      char *user_name = pws->pw_name;
+
+      // get group name
+      struct group *grp = getgrgid(gid);
+      char *group_name = grp->gr_name;
+
+      // get file name
+      int name_start_index = 1;
+      for (size_t i = path_len - 2; name[i] != '/'; --i)
+      {
+         name_start_index++;
+      }
+      name_start_index = path_len - name_start_index;
+      strncpy(file_name, name + name_start_index, path_len - 2);
+
+      // get permissions
+      permissions[0] = 'd';
+      permissions[1] = status->st_mode & S_IRUSR ? 'r' : '-';
+      permissions[2] = status->st_mode & S_IWUSR ? 'w' : '-';
+      permissions[3] = status->st_mode & S_IXUSR ? 'x' : '-';
+      permissions[4] = status->st_mode & S_IRGRP ? 'r' : '-';
+      permissions[5] = status->st_mode & S_IWGRP ? 'w' : '-';
+      permissions[6] = status->st_mode & S_IXGRP ? 'x' : '-';
+      permissions[7] = status->st_mode & S_IROTH ? 'r' : '-';
+      permissions[8] = status->st_mode & S_IWOTH ? 'w' : '-';
+      permissions[9] = status->st_mode & S_IXOTH ? 'x' : '-';
+
+      printf("[%s %s %s\t\t%lld]  %s\n", permissions, user_name, group_name, size, file_name);
    }
       
 
